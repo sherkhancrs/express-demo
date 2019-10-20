8c994abbfc191bc57a149f588cc7606b498d6137
Sherkhan <sherkhancrs@gmail.com>
2019-10-20 15:28:39 +0600

    hello world express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello World!!!");
+});
+
+app.get('/api/courses', (req, res) => {
+  res.send([1, 2, 3]);
+})
+
+app.get('/api/posts/:year/:month', (req, res)=> {
+  // res.send(req.params);
+  res.send(req.query);
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+})
